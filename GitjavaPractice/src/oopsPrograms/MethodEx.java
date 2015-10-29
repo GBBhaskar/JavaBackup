2e01ac41c55f35c58279e4b292bfe96ccdbeb023
MounikaReddyDuvoori <mounika@bpatech.com>
2015-10-29 09:57:15 +0530

    Deleted multiplication method

--- GitjavaPractice/src/oopsPrograms/MethodEx.java
+++ GitjavaPractice/src/oopsPrograms/MethodEx.java
@@ -16,20 +16,13 @@ public void sub()//Method name should start with small letter
 	int r=p-q;
 	System.out.println("addition is:"+r);
  }
-public void mul()//Method name should start with small letter
-{
-	int d=10;
-	int e=20;
-	int f=d*e;
-	System.out.println("addition is:"+f);
-}
+
 	public static void main (String []args)
 	{
 		System.out.println("Example for method in java");
 		MethodEx m=new MethodEx();
 		m.add();
-		m.sub();
-		m.mul();
+		m.sub();;
 	}
 	
 	}
